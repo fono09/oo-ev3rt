71e9a16f265bc888dd9e87b64fbae5936dfa108c
fono09 <fono@fono.jp>
2017-05-31 12:49:33 +0900

    Edit buid.sh / Add README.md

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+APP_NAME=helloev3
+UPLOAD=0
+
 docker build -t fono09/oo_ev3rt .
 docker run -it --rm \
 	-v $PWD/projects:/projects \
 	-v $PWD/upload:/mnt/upload \
-	-e APP_NAME=helloev3 \
-	-e UPLOAD=0 \
+	-e APP_NAME=$APP_NAME \
+	-e UPLOAD=$UPLOAD \
 	fono09/oo_ev3rt:latest
 docker rmi fono09/oo_ev3rt
